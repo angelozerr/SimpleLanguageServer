27754071857cfe871f4b58a1265b05eb072d2551
David Kwon <dakwon@redhat.com>
2019-07-30 11:16:42 -0400

    Implement executeCommand()
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- lsp4quarkus/src/main/java/lsp4quarkus/QuarkusWorkspaceService.java
+++ lsp4quarkus/src/main/java/lsp4quarkus/QuarkusWorkspaceService.java
@@ -1,10 +1,18 @@
 package lsp4quarkus;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Logger;
+
 import org.eclipse.lsp4j.DidChangeConfigurationParams;
 import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
+import org.eclipse.lsp4j.ExecuteCommandParams;
 import org.eclipse.lsp4j.services.WorkspaceService;
 
+import com.google.gson.JsonPrimitive;
+
 public class QuarkusWorkspaceService implements WorkspaceService {
+	
+	private static final Logger LOGGER = Logger.getLogger(QuarkusLanguageServer.class.getName());
 
 	@Override
 	public void didChangeConfiguration(DidChangeConfigurationParams params) {
@@ -17,5 +25,22 @@ public class QuarkusWorkspaceService implements WorkspaceService {
 		// TODO Auto-generated method stub
 		
 	}
+	
+    @Override
+    public CompletableFuture<Object> executeCommand(ExecuteCommandParams params) {
+    	
+    	
+    	
+    	String command = ((JsonPrimitive)params.getArguments().get(0)).getAsString();
+    	
+        return CompletableFuture.supplyAsync(() -> {
+        	switch (command) {
+        		case "sendMetadata":
+        			return "OK";
+        		default:
+        			return "NOT OK";
+        	}
+        });
+    }
 
 }
